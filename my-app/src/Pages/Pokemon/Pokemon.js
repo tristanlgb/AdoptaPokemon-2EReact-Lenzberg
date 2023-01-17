6674a109b580c27b1086b2f7fbcd54485740a6e6
Tristan Lenzberg <112999910+tristanlgb@users.noreply.github.com>
2023-01-17 16:46:11 -0300

    Update Pokemon.js

--- my-app/src/Pages/Pokemon/Pokemon.js
+++ my-app/src/Pages/Pokemon/Pokemon.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Pokemon ({pokemonItem}) {
-  return (
+  console.log(pokemonItem)
+   return (
     <Card style={{ width: '18rem' }}>
        <Card.Body>
         <Card.Title> <h2>{pokemonItem.name}</h2></Card.Title>
         <Card.Text><img src= {pokemonItem.frontImg}/> </Card.Text>
          
-        <Button variant="success" src='../Detail/Detail'>Ver  detalles</Button>
+        <Button variant="success" Link to={ Detail }>Ver  detalles</Button>
       </Card.Body>
     </Card>
   );
