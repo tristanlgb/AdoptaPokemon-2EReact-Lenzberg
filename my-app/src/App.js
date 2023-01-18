947d12885499b4f66c71e918f1e8e8b42dac3ad6
Tristan Lenzberg <112999910+tristanlgb@users.noreply.github.com>
2023-01-18 01:27:21 -0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import ItemListContainer from './Pages/ItemListContainer/ItemListContainer';
 import NavBar from './components/Navbar/NavBar';
-import Detail from './Pages/Detail/Detail';
+import ItemDetail from './Pages/ItemDetail/ItemDetail';
 import Error from './Pages/Error/Error';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -12,7 +12,7 @@ function App () {
     <NavBar />
     <Routes>
         <Route path='/itemlistcontainer' element={<ItemListContainer />} />
-        <Route path='/detail' element={<Detail />} />
+        <Route path='/detail' element={<ItemDetail />} />
         <Route path='*' element={<Error />} />
     </Routes>
     </BrowserRouter>
